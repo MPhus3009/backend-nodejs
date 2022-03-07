cf01ad6d731907c7e184783bca51abc9f27bdac1
Minh Phu <phu3009@gmail.com>
2022-03-07 15:23:52 +0700

    Cai dat cac handler truy xuat CSDL

--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -4,16 +4,12 @@ const contacts = require("../controllers/contact.controller");
 module.exports = (app)=>{
     const router = express.Router();
 
-    //Retrieve all contacts
     router.get("/", contacts.findAll);
 
-    // Create a new contact
     router.post("/", contacts.create);
 
-    // Delete all contacts
     router.delete("/", contacts.deleteAll);
 
-    // Retrieve all favorite contacts
     router.get("/favorite", contacts.findAllFavorite);
 
     router.get("/:id",contacts.findOne);
